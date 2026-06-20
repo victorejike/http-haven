a81e0f56822edceefd2d4b4c6c1e5951fe6ffd99
victor ejike <victorejiki@gmail.com>
2026-06-20 19:13:12 +0100

    server

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 
 	http.HandleFunc("/ping", server.Homeserver)
+	http.HandleFunc("/hello", server.Hello)
 
 	println("server is now live")
 	http.ListenAndServe(":8080", nil)
--- server/handle.go
+++ server/handle.go
@@ -1,11 +1,25 @@
 package server
 
 import (
-	
 	"fmt"
 	"net/http"
 )
 
-func Homeserver(w http.ResponseWriter, r *http.Request){
+func Homeserver(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	name := r.URL.Query().Get("name")
+	// Default to "Guest" if name parameter is missing
+	if name == "" {
+		name = "Guest"
+	}
+
+	// Respond with greeting
+	fmt.Fprintf(w, "Hello, %s!", name)
 }
